5b10a033aa1e122a813e5bc28022a16e210eff85
Natallia Kapitan <natallia.kapitann@gmail.com>
2025-10-23 23:07:54 +0300

    feat: complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  const doubleNext = '--double-next';
+  const discardPrev = '--discard-prev';
+  const discardNext = '--discard-next';
+  const doublePrev = '--double-prev';
+
+  const newArray = [...arr];
+
+  for (let index = 0; index < newArray.length; index++) {
+    switch (newArray[index]) {
+      case doubleNext:
+        if (index < newArray.length - 1) {
+          newArray.splice(index, 1, newArray[index + 1]);
+        } else {
+            newArray.splice(index, 1);
+          }
+        break;
+
+      case discardPrev:
+        if (index === 0) {
+          newArray.splice(index, 1);
+        } else {
+          newArray.splice(index - 1, 2);
+        }
+        break;
+
+      case discardNext:
+        if (index === newArray.length) {
+          newArray.splice(index, 1);
+        } else {
+          newArray.splice(index, 2);
+          index = index - 1;
+        }
+        break;
+
+      case doublePrev:
+        if (index === 0) {
+          newArray.splice(index, 1);
+        } else {
+          newArray.splice(index, 1, newArray[index - 1]);
+        }
+        break;
+    }
+  }
+
+  return newArray;
 }
 
 module.exports = {
